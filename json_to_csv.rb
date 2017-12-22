90fedf08b1122717c04d8df368b1e0486640cd22
Raphael FOSSE <raphael.fosse91@gmail.com>
2017-12-22 14:48:19 +0100

    adding explanations

--- json_to_csv.rb
+++ json_to_csv.rb
@@ -2,6 +2,8 @@ require 'csv'
 require 'json'
 require 'set'
 
+# First step : Getting headers
+
 def get_recursive_keys(hash, nested_key=nil)
   hash.each_with_object([]) do |(k,v),keys|
     k = "#{nested_key}.#{k}" unless nested_key.nil?
@@ -19,15 +21,20 @@ json.each do |hash|
   headings.merge(get_recursive_keys(hash))
 end
 
+# Step 2 : Convert heading into an array to populate it
+
 headings = headings.to_a
+
+# Step 3 : Use of dig method to look for nested values & join method on array to transformed as a string.
 
 CSV.open('file3.csv', 'w') do |csv|
   csv << headings
   json.each do |hash|
     row = headings.map do |h|
-      v = hash.dig(*h.split('.'))       # Dig out the (possibly) nested value
-      v.is_a?(Array) ? v.join(',') : v  # Fix up arrays
+      v = hash.dig(*h.split('.'))
+      v.is_a?(Array) ? v.join(',') : v
     end
+    # Step 4 : Pushing row into a csv file.
     csv << row
   end
 end
